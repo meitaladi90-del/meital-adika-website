980389571e54196ba1300d942132289b8d6089dd
meitaladi90-del <meitaladi90@gmail.com>
2026-05-05 13:39:46 +0300

    fix: src/lib/stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-04-10",
-  typescript: true,
+  apiVersion: "2023-10-16",
 });
 
 export async function createCheckoutSession({
