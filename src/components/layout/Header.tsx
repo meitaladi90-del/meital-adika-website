3e75be9c5c895eb15e17f2fc300dde29ba3f426b
meitaladi90-del <meitaladi90@gmail.com>
2026-06-06 22:08:03 +0300

    fix: remove emojis from src/components/layout/Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -125,7 +125,6 @@ export default function Header() {
                     }}
                     onClick={() => setIsOpen(false)}
                   >
-                    <span>✨</span>
                     <span>האנרגיה היומית שלך</span>
                   </Link>
                 </li>
